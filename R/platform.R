a1a01616e3d44a7cae182e772c41ec6739ccd979
Minghao Jiang <jiangminghao1001@163.com>
2022-09-29 16:57:08 +0800

    add additional files

---
+++ R/platform.R
@@ -0,0 +1,9 @@
+#' platform: A package to render QC reports for research groups at SII.
+#' @description platform: A package to render QC reports for research groups at SII.
+#' @docType package
+#' @name platform-package
+NULL
+
+
+
+
