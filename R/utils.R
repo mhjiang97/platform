7499cfe80ab8469c6ff0bc0e0f55e964cbd8e836
Minghao Jiang <jiangminghao1001@163.com>
2022-10-03 13:17:22 +0800

    Update utils.R

--- R/utils.R
+++ R/utils.R
@@ -1,7 +1,7 @@
 #' @importFrom glue glue
 myzip <- function(dir, name = "report", ...) {
   cmd <- glue::glue(
-    "tar -zcvf {dirname(dir_out)}/{name}.tar.gz {dir_out}"
+    "tar -zcvf {dirname(dir)}/{name}.tar.gz {dir}"
   )
 
   system(cmd)
